cd1a2bb3b27489ee09fcdb86a681a479df379463
Monika Gustilo <monikagustilo@gmail.com>
2022-07-09 21:43:06 -0700

    commit tree 60b8bbae9ee3e94cdf62c13ed92faac31fbdfc33

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+const titleCased = function () {
+  for (let i = 0; i < tutorials.length; i++) {
+    tutorials[i] = tutorials[i]
+      .split(" ")
+      .map(function (element) {
+        return (
+          element.charAt(0).toUpperCase() + element.slice(1, element.length)
+        );
+      })
+      .join(" ");
+  }
+  return tutorials;
+};
+
+console.log(titleCased());
